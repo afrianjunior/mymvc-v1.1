4a5af645843006d96e89d430e1b3288caf8cf83b
Adiyatma <adiyatma@Adiyatmas-MacBook-Pro.local>
2016-09-15 10:53:13 +0800

    add comment and a little function

--- helpers.php
+++ helpers.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+* ----------------------------------------------------------------------------------
+* Handle redirect to directory views/
+* ----------------------------------------------------------------------------------
+*/
+
 if(!function_exists('view')){
 	function view($viewPath, array $data = [])
 	{
@@ -21,6 +27,12 @@ if(!function_exists('view')){
 	}
 }
 
+/**
+* ----------------------------------------------------------------------------------
+* Handle convert env.json to array
+* ----------------------------------------------------------------------------------
+*/
+
 if(!function_exists('env')){
 	function env($key){
 		if(!is_string($key)){
@@ -29,5 +41,42 @@ if(!function_exists('env')){
 		$json = file_get_contents('env.json');
 		$data = (array) json_decode($json);
 		return $data[$key];
+	}
+}
+
+/**
+* ----------------------------------------------------------------------------------
+* Handle convert string like Hello World to hello-world for uri
+* ----------------------------------------------------------------------------------
+*/
+
+if(!function_exists('slug')){
+	function slug($str){
+		$str = preg_replace('/[^a-zA-Z0-9-]+/', '-', strtolower($str));
+		return $str;
+	}
+}
+
+/**
+* ----------------------------------------------------------------------------------
+* Handle assets for redirect to directory assets/
+* ----------------------------------------------------------------------------------
+*/
+
+if(!function_exists('assets')){
+	function assets($path){
+		return BASE_URL . 'assets/' . $path;
+	}
+}
+
+/**
+* ----------------------------------------------------------------------------------
+* Handle resources for get a files from directory app/resource/uploads
+* ----------------------------------------------------------------------------------
+*/
+
+if(!function_exists('uploads')){
+	function uploads($file){
+		return BASE_URL . 'app/resources/uploads' . $file;
 	}
 }
