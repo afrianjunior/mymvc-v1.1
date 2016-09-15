3ea30c23d22e9a4f91ef5233ba54eef1c5ede07f
Adiyatma <adiyatma@Adiyatmas-MacBook-Pro.local>
2016-09-15 13:22:46 +0800

    add comment to Session

--- app/core/Session.php
+++ app/core/Session.php
@@ -2,16 +2,34 @@
 
 class Session
 {
+	/**
+	* Handle put session
+	*
+	* @param $name string
+	* @param $value string
+	*/
 	public static function put($name, $value)
 	{
 		return $_SESSION[$name] = $value;
 	}
 
+	/**
+	* Handle get session
+	*
+	* @param $name string
+	* @return string
+	*/
 	public static function get($name)
 	{
 		return $_SESSION[$name];
 	} 
 
+	/**
+	* Handle delete session value
+	*
+	* @param $name string
+	* @return unset
+	*/
 	public static function delete($name)
 	{
 		if(self::exists($name)){
@@ -19,11 +37,24 @@ class Session
 		}
 	}
 
+	/**
+	* Handle check session value
+	*
+	* @param $name string
+	* @return bool true | false
+	*/
 	public static function exists($name)
 	{
 		return (isset($_SESSION[$name])) ? true : false;
 	}
 
+	/**
+	* Handle show session with value
+	*
+	* @param $name string
+	* @param $message string
+	* @return string
+	*/
 	public static function flash($name, $message = '')
 	{
 		if(self::exists($name)){
